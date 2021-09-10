6667f63559df0ba3c2530054fb979e713caa8b3e
Ricardo Čerljenko <ricardo@lloyds.design>
2021-09-10 13:49:50 +0200

    initial commit

--- src/LaravelJwtServiceProvider.php
+++ src/LaravelJwtServiceProvider.php
@@ -3,18 +3,33 @@
 namespace RCerljenko\LaravelJwt;
 
 use Illuminate\Support\ServiceProvider;
+use RCerljenko\LaravelJwt\Guard\JwtGuard;
 
 class LaravelJwtServiceProvider extends ServiceProvider
 {
 	public function boot()
+	{
+		$this->publishConfig();
+
+		$this->extendAuthGuard();
+	}
+
+	public function register()
+	{
+		$this->mergeConfigFrom(__DIR__ . '/../config/jwt.php', 'jwt');
+	}
+
+	private function publishConfig()
 	{
 		$this->publishes([
 			__DIR__ . '/../config/jwt.php' => config_path('jwt.php'),
 		], 'config');
 	}
 
-	public function register()
+	private function extendAuthGuard()
 	{
-		$this->mergeConfigFrom(__DIR__ . '/../config/jwt.php', 'jwt');
+		auth()->extend('jwt', function ($app, $name, array $config) {
+			return new JwtGuard(auth()->createUserProvider($config['provider']));
+		});
 	}
 }
